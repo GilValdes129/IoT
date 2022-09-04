0bec3498022d66b93931fc95379ba1f10d71898d
GilValdes129 <70418863+GilValdes129@users.noreply.github.com>
2022-09-03 20:29:15 -0500

    Agregar estilos

--- app.js
+++ app.js
@@ -11,13 +11,22 @@ const est = document.getElementById("Estado")
 
 if (randomizer >= 60 && randomizer <= 100) {
     est.innerHTML = "normal"
+    est.style.backgroundColor = "green"
 }
 else if (randomizer >= 101 && randomizer <= 110) {
     est.innerHTML = "alta"
+    est.style.backgroundColor = "red"
 }
 else if (randomizer >= 111) {
     est.innerHTML = "extrema"
+    est.style.backgroundColor = "#800000"
 }
 else if (randomizer <= 59) {
     est.innerHTML = "baja"
+    est.style.backgroundColor = "blue"
 }
+
+var minutes = 5
+var array = [1,2,3,4,5]
+
+console.log(array)
